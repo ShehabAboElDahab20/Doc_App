bd66e7d696ab8165655efd4fdc4da68ec047ff03
ShehabAboElDahab20 <shehabxuwk@gmail.com>
2025-11-06 02:17:43 +0200

    fix: remove unnecessary whitespace in DocApp build method

--- doc_app/lib/doc_app.dart
+++ doc_app/lib/doc_app.dart
@@ -10,6 +10,7 @@ class DocApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
+      
       designSize: const Size(375, 812),
       minTextAdapt: true,
       child: MaterialApp.router(
